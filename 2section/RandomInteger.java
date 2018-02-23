5a4a1f7efa773dead2ce6daae27547268359ad49
nekousagi-chun <pkedxhghicee4m2t3gtk@docomo.ne.jp>
2018-02-23 13:03:12 +0900

    乱数生成

---
+++ 2section/RandomInteger.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+class RandomInteger{
+
+  public static void main(String[] args){
+    Random rand = new Random();
+    int n1 = 1 + rand.nextInt(9);
+    int n2 = -1 - rand.nextInt(9);
+    int n3 = 10 + rand.nextInt(90);
+
+    System.out.println("3個の乱数を生成しました．");
+    System.out.println("1桁の正の整数:" + n1);
+    System.out.println("1桁の負の整数:" + n2);
+    System.out.println("2桁の正の整数:" + n3);
+  }
+}
